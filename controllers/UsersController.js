baa7c10e1304695e5b167d628d1490f704f4c9a4
Mahmoud Ali Mohamed <45719022+mahmoudali94@users.noreply.github.com>
2024-09-05 01:20:16 +0300

    controllers

---
+++ controllers/UsersController.js
@@ -0,0 +1,77 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+const RedisClient = require('../utils/redis');
+const { ObjectId } = require('mongodb');
+
+class UsersController {
+  static postNew(request, response) {
+    const { email } = request.body;
+    const { password } = request.body;
+
+    if (!email) {
+      response.status(400).json({ error: 'Missing email' });
+      return;
+    }
+    if (!password) {
+      response.status(400).json({ error: 'Missing password' });
+      return;
+    }
+
+    const users = dbClient.db.collection('users');
+    users.findOne({ email }, (err, user) => {
+      if (user) {
+        response.status(400).json({ error: 'Already exist' });
+      } else {
+        const hashedPassword = sha1(password);
+        users.insertOne(
+          {
+            email,
+            password: hashedPassword,
+          },
+        ).then((result) => {
+          response.status(201).json({ id: result.insertedId, email });
+
+        }).catch((error) => console.log(error));
+      }
+    });
+  }
+
+  static async getMe(req, res){
+    try {
+      const token  = req.headers['x-token']
+      if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+        
+      }else{
+        const id = await RedisClient.get(`auth_${token}`)
+  
+        if (id) {
+          const usersCollection = dbClient.db.collection('users')
+          const user = await usersCollection.findOne({_id: ObjectId(id)})
+          if (user) {
+            res.status(200).json({ id: user._id, email: user.email });
+          } else {
+            res.status(401).json({ error: 'Unauthorized' });
+          }
+  
+          
+        }
+        else{
+          res.status(401).json({ error: 'Unauthorized' });
+
+
+        }
+  
+  
+      }
+      
+    } catch (error) {
+      console.log(error)
+      res.status(401).json({ error: 'Unauthorized' });
+      
+    }
+   
+  }
+}
+
+module.exports = UsersController;
