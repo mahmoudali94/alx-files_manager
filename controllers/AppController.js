b1cbed801e2cc8e560051aeccc5e514884d6257c
Mahmoud Ali Mohamed <45719022+mahmoudali94@users.noreply.github.com>
2024-09-05 01:19:41 +0300

    Create AppController.js

---
+++ controllers/AppController.js
@@ -0,0 +1,18 @@
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+  static async getStatus(request, response) {
+    response.status(200).json({ 
+      redis: await redisClient.isAlive(), 
+      db: await dbClient.isAlive() });
+  }
+
+  static async getStats(request, response) {
+    const usersNum = await dbClient.nbUsers();
+    const filesNum = await dbClient.nbFiles();
+    response.status(200).json({ users: usersNum, files: filesNum });
+  }
+}
+
+module.exports = AppController;
